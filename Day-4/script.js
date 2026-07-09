b79b8dd501df5b26ea25c0c576aeb2695d050dde
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2026-07-09 23:10:36 +0530

    4th day of level up part 1

---
+++ Day-4/script.js
@@ -0,0 +1,48 @@
+
+/*
+
+
+1. Prints numbers from 1 to 100 using a for loop.
+2. Prints even numbers from 1 to 50.
+3. Uses a while loop for a countdown from 10 to 1.
+4. Uses a do...while loop that runs at least once.
+5. Iterates through an array using for...of.
+6. Iterates through an object using for...in.
+7. Demonstrates break and continue.
+
+*/
+
+//! for loop
+//! Q1. 
+
+// for(let i = 0; i <= 100; i++){
+//     console.log(i);
+// }
+
+
+// Q2
+
+// for(let i = 0; i <= 50; i++){
+//     if(!(i % 2)) console.log(i);
+// }
+
+// while loop
+// Q3
+
+// let i = 10;
+// while (i > 0) {
+//     console.log(i);
+//     --i;
+// }
+
+
+//! do while
+// Q4.
+
+let i = 101;
+
+do {
+
+    console.log("Run ones", i)
+
+} while (i <= 100)
